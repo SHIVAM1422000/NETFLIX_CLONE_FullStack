fa5e62f0fef4002aa92172772571a3b8d12ead52
Shivam Ojha <shivamojha@Shivams-MacBook-Air.local>
2025-09-04 17:02:50 +0530

    Completed Nav and Login , Logout, SignUp Componrnt till 1:12:43

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
         <Routes>
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/netflix" element={<Netflix />} />
+          <Route exact path="/" element={<Netflix />} />
         </Routes>
       </BrowserRouter>
     </>
